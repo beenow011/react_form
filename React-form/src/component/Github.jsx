bc0514faba3ca84ad43cd3e8bd8be9738eb67518
beenow011 <abhinavnb11@gmail.com>
2023-11-30 08:15:59 +0530

    final-1

--- React-form/src/component/Github.jsx
+++ React-form/src/component/Github.jsx
@@ -5,12 +5,11 @@ const Github = () => {
     <div className="">
       <a href="https://github.com/beenow011/react_form">
         <img
-          src="../src/assets/github.webp"
+          src="../src/assets/github2.png"
           alt=""
-          srcset=""
           width={200}
           height={300}
-          className="rounded-full absolute right-[30%] shadow-2xl shadow-black hover:shadow-purple-950"
+          className="rounded-full absolute right-[30%] shadow-2xl shadow-black hover:shadow-purple-950 bg-blue-800 w-24 h-24"
         />
       </a>
     </div>
